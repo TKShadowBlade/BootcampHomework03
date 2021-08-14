3bafdd4f7cf7753be957ad1d7a4634965aa92b84
TKShadowBlade <katanablack85@gmail.com>
2021-08-14 15:28:56 -0400

    updated with coding

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,129 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+var upperCasedCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+];
+var lowerCasedCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+];
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.',
+];
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var chosenChars = []
+var password = ""
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  
   // write prompt for password length between 8 and 128 chars
-  
-  // write 4 confirms for character types lowercase, uppercase, numbers,
+let pwLength = prompt("How many characters between 8 and 128?") 
+  if(pwLength=null){
+    alert("Error: Selection Invalid")
+  }
+
+// write 4 confirms for character types lowercase, uppercase, numbers,
   // and special characters
+  let lowerCaseConfirm = confirm("Include Lower Case?")
+  let upperCaseConfirm = confirm("Include Upper Case?")
+  let numberConfirm = confirm("Include Numbers?")
+  let specialConfirm = confirm("Include Special Characters?")
+  
+  // set conditions for password character selection
+  if (lowerCaseConfirm){
+    chosenChars = chosenChars.concat(lowerCasedCharacters)
+  }
+  if (upperCaseConfirm){
+    chosenChars = chosenChars.concat(upperCasedCharacters)
+  }
+  if (numberConfirm){
+    chosenChars = chosenChars.concat(numericCharacters)
+  }
+  if (specialConfirm){
+    chosenChars = chosenChars.concat(specialCharacters)
+  }
+  for(let i = 0; i < pwLength; i++){
+    var randIndex = Math.floor(Math.random() * chosenChars.length);
+  var randElement = chosenChars[randIndex];
+  password += randElement;
+  }
+
+  var passwordText = document.querySelector("#password");
+  passwordText.textContent = password
+
+  
+
   // make conditional statement to move through confirms
   // add alert if nothing is selected, saying at least one must be picked
 
@@ -22,6 +135,7 @@ function writePassword() {
 
   
 }
+function generatePassword(){}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
